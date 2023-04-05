08505ecd424c9bb211af5ecc3adee16cf4fa74bb
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-04-05 22:41:37 +0200

    Add docs to FFI API

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use once_cell::sync::OnceCell;
 use std::{ffi::c_void, ptr::null_mut};
 use tracing::debug;
 
+const MH_ALL_HOOKS: *const i32 = std::ptr::null();
+
 #[allow(non_camel_case_types)]
 #[must_use]
 #[repr(C)]
@@ -45,9 +47,11 @@ extern "system" {
     /// Initializes the MinHook library. You must call this function in the
     /// beginning of your program.
     fn MH_Initialize() -> MH_STATUS;
+
     /// Uninitialize the MinHook library. You must call this function EXACTLY
     /// ONCE at the end of your program.
     fn MH_Uninitialize() -> MH_STATUS;
+
     /// Creates a hook for the specified target function, in disabled state.
     ///
     /// # Arguments
@@ -60,12 +64,31 @@ extern "system" {
         pDetour: *mut c_void,
         ppOriginal: *mut *mut c_void,
     ) -> MH_STATUS;
+
+    /// Creates a hook for the specified API function, in disabled state.
+    ///
+    /// # Arguments
+    ///
+    /// * `pszModule` \[in\] - A pointer to the loaded module name which contains the target function.
+    /// * `pszProcName` \[in\] - A pointer to the target function name, which will be overridden by the detour function.
+    /// * `pDetour` \[in\] - A pointer to the detour function, which will override the target function.
+    /// * `ppOriginal` \[out\] - A pointer to the trampoline function, which will be used to call the original target function. This parameter can be NULL.
     fn MH_CreateHookApi(
         pszModule: *const u8,
         pszProcName: *const u8,
         pDetour: *mut c_void,
         ppOriginal: *mut *mut c_void,
     ) -> MH_STATUS;
+
+    /// Creates a hook for the specified API function, in disabled state.
+    ///
+    /// # Arguments
+    ///
+    /// * `pszModule` \[in\] - A pointer to the loaded module name which contains the target function.
+    /// * `pszProcName` \[in\] - A pointer to the target function name, which will be overridden by the detour function.
+    /// * `pDetour` \[in\] - A pointer to the detour function, which will override the target function.
+    /// * `ppOriginal` \[out\] - A pointer to the trampoline function, which will be used to call the original target function. This parameter can be NULL.
+    /// * `ppTarget` \[out\] - A pointer to the target function, which will be overridden by the detour function. This parameter can be NULL.
     fn MH_CreateHookApiEx(
         pszModule: *const u8,
         pszProcName: *const u8,
@@ -73,11 +96,43 @@ extern "system" {
         ppOriginal: *mut *mut c_void,
         ppTarget: *mut *mut c_void,
     ) -> MH_STATUS;
+
+    /// Removes an already created hook.
+    ///
+    /// # Arguments
+    ///
+    /// * `pTarget` \[in\] - A pointer to the target function.
     fn MH_RemoveHook(pTarget: *mut c_void) -> MH_STATUS;
+
+    /// Enables an already created hook.
+    ///
+    /// # Arguments
+    ///
+    /// * `pTarget` \[in\] - A pointer to the target function.
     fn MH_EnableHook(pTarget: *mut c_void) -> MH_STATUS;
+
+    /// Disables an already created hook.
+    ///
+    /// # Arguments
+    ///
+    /// * `pTarget` \[in\] - A pointer to the target function.
     fn MH_DisableHook(pTarget: *mut c_void) -> MH_STATUS;
+
+    /// Queues to enable an already created hook.
+    ///
+    /// # Arguments
+    ///
+    /// * `pTarget` \[in\] - A pointer to the target function.
     fn MH_QueueEnableHook(pTarget: *mut c_void) -> MH_STATUS;
+
+    /// Queues to disable an already created hook.
+    ///
+    /// # Arguments
+    ///
+    /// * `pTarget` \[in\] - A pointer to the target function.
     fn MH_QueueDisableHook(pTarget: *mut c_void) -> MH_STATUS;
+
+    /// Applies all queued changes in one go.
     fn MH_ApplyQueued() -> MH_STATUS;
 }
 
