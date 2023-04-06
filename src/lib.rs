971a0821574d8e156008418a8a02a93cbddd9939
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-04-06 15:45:20 +0200

    Add CI, struct for MinHook funcs, redo tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,214 @@
 #![allow(dead_code, non_snake_case, non_camel_case_types)]
 
 use once_cell::sync::OnceCell;
-use std::{ffi::c_void, ptr::null_mut};
+use std::{
+    ffi::{c_void, CString},
+    ptr::null_mut,
+};
 use tracing::debug;
 
 const MH_ALL_HOOKS: *const i32 = std::ptr::null();
+
+static MINHOOK_INIT: OnceCell<()> = OnceCell::new();
+static MINHOOK_UNINIT: OnceCell<()> = OnceCell::new();
+
+pub struct MinHook {}
+
+impl MinHook {
+    unsafe fn initialize() {
+        MINHOOK_INIT.get_or_init(|| unsafe {
+            let status = MH_Initialize();
+            debug!("MH_Initialize: {:?}", status);
+
+            status.ok().expect("Couldn't initialize MinHook");
+        });
+    }
+
+    /// # Safety
+    pub unsafe fn uninitialize() {
+        // Make sure we are initialized
+        Self::initialize();
+
+        MINHOOK_UNINIT.get_or_init(|| unsafe {
+            let status = MH_Uninitialize();
+            debug!("MH_Uninitialize: {:?}", status);
+
+            status.ok().expect("Couldn't uninitialize MinHook");
+        });
+    }
+
+    /// # Safety
+    pub unsafe fn create_hook(
+        target: *mut c_void,
+        detour: *mut c_void,
+    ) -> Result<*mut c_void, MH_STATUS> {
+        Self::initialize();
+
+        let mut original: *mut c_void = null_mut();
+        let status = unsafe { MH_CreateHook(target, detour, &mut original) };
+        debug!("create_hook: {:?} {:?} {:?}", target, detour, original);
+        match status {
+            MH_STATUS::MH_OK => Ok(original),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn enable_hook(target: *mut c_void) -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let status = unsafe { MH_EnableHook(target) };
+        debug!("enable_hook: {:?} {:?}", target, status);
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn enable_all_hooks() -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let status = unsafe { MH_EnableHook(MH_ALL_HOOKS as *mut _) };
+        debug!("enable_all_hooks: {:?}", status);
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn disable_hook(target: *mut c_void) -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let status = unsafe { MH_DisableHook(target) };
+        debug!("disable_hook: {:?} {:?}", target, status);
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn disable_all_hooks() -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let status = unsafe { MH_DisableHook(MH_ALL_HOOKS as *mut _) };
+        debug!("disable_all_hooks: {:?}", status);
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn remove_hook(target: *mut c_void) -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let status = unsafe { MH_RemoveHook(target) };
+        debug!("remove_hook: {:?} {:?}", target, status);
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn queue_enable_hook(target: *mut c_void) -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let status = unsafe { MH_QueueEnableHook(target) };
+        debug!("queue_enable_hook: {:?} {:?}", target, status);
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn queue_disable_hook(target: *mut c_void) -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let status = unsafe { MH_QueueDisableHook(target) };
+        debug!("queue_disable_hook: {:?} {:?}", target, status);
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn apply_queued() -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let status = unsafe { MH_ApplyQueued() };
+        debug!("apply_queued: {:?}", status);
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn create_hook_api(
+        module_name: &str,
+        proc_name: &str,
+        detour: *mut c_void,
+    ) -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let module_name = CString::new(module_name).unwrap();
+        let proc_name = CString::new(proc_name).unwrap();
+        let mut original: *mut c_void = null_mut();
+        let status = unsafe {
+            MH_CreateHookApi(
+                module_name.as_ptr() as *const _,
+                proc_name.as_ptr() as *const _,
+                detour,
+                &mut original,
+            )
+        };
+        debug!(
+            "create_hook_api: {:?} {:?} {:?} {:?}",
+            module_name, proc_name, detour, original
+        );
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+
+    /// # Safety
+    pub unsafe fn create_hook_api_ex(
+        module_name: &str,
+        proc_name: &str,
+        detour: *mut c_void,
+        pp_target: *mut *mut c_void,
+    ) -> Result<(), MH_STATUS> {
+        Self::initialize();
+
+        let module_name = CString::new(module_name).unwrap();
+        let proc_name = CString::new(proc_name).unwrap();
+        let mut original: *mut c_void = null_mut();
+        let status = unsafe {
+            MH_CreateHookApiEx(
+                module_name.as_ptr() as *const _,
+                proc_name.as_ptr() as *const _,
+                detour,
+                &mut original,
+                pp_target,
+            )
+        };
+        debug!(
+            "create_hook_api_ex: {:?} {:?} {:?} {:?} {:?}",
+            module_name, proc_name, detour, original, pp_target
+        );
+        match status {
+            MH_STATUS::MH_OK => Ok(()),
+            _ => Err(status),
+        }
+    }
+}
 
 #[allow(non_camel_case_types)]
 #[must_use]
@@ -257,90 +461,118 @@ impl Drop for MhHooks {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::mem::transmute;
+    use std::{ffi::c_void, mem};
 
     #[test]
-    fn test_hooks() {
+    fn test_hook() {
         unsafe {
-            let hooks = MhHooks::new([
-                MhHook::new(
-                    transmute::<_, *mut c_void>(test_fn1 as fn() -> i32),
-                    transmute::<_, *mut c_void>(test_fn1_hook as fn() -> i32),
-                )
-                .unwrap(),
-                MhHook::new(
-                    transmute::<_, *mut c_void>(test_fn2 as fn(i32) -> i32),
-                    transmute::<_, *mut c_void>(test_fn2_hook as fn(i32) -> i32),
-                )
-                .unwrap(),
-            ])
+            MinHook::create_hook(
+                test_fn as FnType as *mut c_void,
+                test_fn_hook as FnType as *mut c_void,
+            )
             .unwrap();
 
-            hooks.apply();
+            // Test that the hook is enabled.
+            MinHook::enable_hook(test_fn as FnType as *mut c_void).unwrap();
+            assert_eq!(test_fn(), 1);
+
+            // Test that the hook is disabled.
+            MinHook::disable_hook(test_fn as FnType as *mut c_void).unwrap();
+            assert_eq!(test_fn(), 0);
+        }
+
+        type FnType = fn() -> i32;
+
+        fn test_fn() -> i32 {
+            0
+        }
+
+        fn test_fn_hook() -> i32 {
+            1
+        }
+    }
+
+    #[test]
+    fn test_hooks_queue() {
+        unsafe {
+            MinHook::create_hook(
+                test_fn1 as fn() -> i32 as *mut c_void,
+                test_fn1_hook as fn() -> i32 as *mut c_void,
+            )
+            .unwrap();
+            MinHook::create_hook(
+                test_fn2 as fn(i32) -> i32 as *mut c_void,
+                test_fn2_hook as fn(i32) -> i32 as *mut c_void,
+            )
+            .unwrap();
+
+            MinHook::queue_enable_hook(test_fn1 as fn() -> i32 as *mut c_void).unwrap();
+            MinHook::queue_enable_hook(test_fn2 as fn(i32) -> i32 as *mut c_void).unwrap();
+            MinHook::apply_queued().unwrap();
 
             // Test that the hooks are applied.
             assert_eq!(test_fn1(), 1);
             assert_eq!(test_fn2(1), 2);
 
-            hooks.unapply();
+            MinHook::queue_disable_hook(test_fn1 as fn() -> i32 as *mut c_void).unwrap();
+            MinHook::queue_disable_hook(test_fn2 as fn(i32) -> i32 as *mut c_void).unwrap();
+            MinHook::apply_queued().unwrap();
 
             // Test that the hooks are unapplied.
             assert_eq!(test_fn1(), 0);
             assert_eq!(test_fn2(1), 1);
         }
-    }
 
-    fn test_fn1() -> i32 {
-        0
-    }
+        fn test_fn1() -> i32 {
+            0
+        }
 
-    fn test_fn1_hook() -> i32 {
-        1
-    }
+        fn test_fn1_hook() -> i32 {
+            1
+        }
 
-    fn test_fn2(x: i32) -> i32 {
-        x
-    }
+        fn test_fn2(x: i32) -> i32 {
+            x
+        }
 
-    fn test_fn2_hook(x: i32) -> i32 {
-        x + 1
-    }
-
-    type FnType = fn() -> i32;
-    static TRAMPOLINE: OnceCell<FnType> = OnceCell::new();
-
-    fn test_fn_trampoline_orig() -> i32 {
-        21
-    }
-
-    fn test_fn_trampoline_hook() -> i32 {
-        let _val = 42;
-
-        // ... more would go here ...
-
-        // Call the trampoline function.
-        let trampoline = TRAMPOLINE.get().unwrap();
-        trampoline()
+        fn test_fn2_hook(x: i32) -> i32 {
+            x + 1
+        }
     }
 
     #[test]
-    fn test_hooks_trampoline() {
+    fn test_hook_trampoline() {
         unsafe {
-            let hook = MhHook::new(
-                transmute::<_, *mut c_void>(test_fn_trampoline_orig as fn() -> i32),
-                transmute::<_, *mut c_void>(test_fn_trampoline_hook as fn() -> i32),
+            // Create a hook for `test_fn_trampoline_orig`
+            let trampoline = MinHook::create_hook(
+                test_fn_trampoline_orig as fn(i32) -> i32 as *mut c_void,
+                test_fn_trampoline_hook as fn(i32) -> i32 as *mut c_void,
             )
             .unwrap();
 
-            TRAMPOLINE.get_or_init(|| std::mem::transmute(hook.trampoline()));
+            // Store the trampoline function.
+            TRAMPOLINE.get_or_init(|| mem::transmute(trampoline));
 
-            let hooks = MhHooks::new([hook]).unwrap();
+            // Enable the hook.
+            MinHook::enable_hook(test_fn_trampoline_orig as fn(i32) -> i32 as *mut c_void).unwrap();
 
-            hooks.apply();
+            assert_eq!(test_fn_trampoline_orig(69), 42)
+        }
 
-            assert_eq!(test_fn_trampoline_orig(), 21);
+        type FnType = fn(i32) -> i32;
+        static TRAMPOLINE: OnceCell<FnType> = OnceCell::new();
 
-            hooks.unapply();
+        fn test_fn_trampoline_orig(x: i32) -> i32 {
+            x
+        }
+
+        fn test_fn_trampoline_hook(_x: i32) -> i32 {
+            // Set a value that we want to return for the test.
+            let val = 42;
+
+            // Call the trampoline function.
+            let trampoline = TRAMPOLINE.get().unwrap();
+            trampoline(val)
         }
     }
 }
