79d7668c36b1abe92d4a265c61645d889c22383e
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-04-12 00:54:29 +0200

    Specify type for minhook

--- src/lib.rs
+++ src/lib.rs
@@ -299,19 +299,19 @@ mod tests {
     fn test_hooks_queue() {
         unsafe {
             MinHook::create_hook(
-                test_fn1 as fn() -> i32 as *mut c_void,
-                test_fn1_hook as fn() -> i32 as *mut c_void,
+                test_fn1 as FnType1 as *mut c_void,
+                test_fn1_hook as FnType1 as *mut c_void,
             )
             .unwrap();
             MinHook::create_hook(
-                test_fn2 as fn(i32) -> i32 as *mut c_void,
-                test_fn2_hook as fn(i32) -> i32 as *mut c_void,
+                test_fn2 as FnType2 as *mut c_void,
+                test_fn2_hook as FnType2 as *mut c_void,
             )
             .unwrap();
 
             // Queue to enable the hooks, then apply them.
-            MinHook::queue_enable_hook(test_fn1 as fn() -> i32 as *mut c_void).unwrap();
-            MinHook::queue_enable_hook(test_fn2 as fn(i32) -> i32 as *mut c_void).unwrap();
+            MinHook::queue_enable_hook(test_fn1 as FnType1 as *mut c_void).unwrap();
+            MinHook::queue_enable_hook(test_fn2 as FnType2 as *mut c_void).unwrap();
             MinHook::apply_queued().unwrap();
 
             // Test that the hooks are enabled.
@@ -319,14 +319,17 @@ mod tests {
             assert_eq!(test_fn2(1), 2);
 
             // Queue to disable the hooks, then apply them.
-            MinHook::queue_disable_hook(test_fn1 as fn() -> i32 as *mut c_void).unwrap();
-            MinHook::queue_disable_hook(test_fn2 as fn(i32) -> i32 as *mut c_void).unwrap();
+            MinHook::queue_disable_hook(test_fn1 as FnType1 as *mut c_void).unwrap();
+            MinHook::queue_disable_hook(test_fn2 as FnType2 as *mut c_void).unwrap();
             MinHook::apply_queued().unwrap();
 
             // Test that the hooks are disabled.
             assert_eq!(test_fn1(), 0);
             assert_eq!(test_fn2(1), 1);
         }
+
+        type FnType1 = fn() -> i32;
+        type FnType2 = fn(i32) -> i32;
 
         fn test_fn1() -> i32 {
             0
@@ -350,8 +353,8 @@ mod tests {
         unsafe {
             // Create a hook for `test_fn_trampoline_orig`
             let trampoline = MinHook::create_hook(
-                test_fn_trampoline_orig as fn(i32) -> i32 as *mut c_void,
-                test_fn_trampoline_hook as fn(i32) -> i32 as *mut c_void,
+                test_fn_trampoline_orig as FnType as *mut c_void,
+                test_fn_trampoline_hook as FnType as *mut c_void,
             )
             .unwrap();
 
@@ -359,7 +362,7 @@ mod tests {
             TRAMPOLINE.get_or_init(|| mem::transmute(trampoline));
 
             // Enable the hook.
-            MinHook::enable_hook(test_fn_trampoline_orig as fn(i32) -> i32 as *mut c_void).unwrap();
+            MinHook::enable_hook(test_fn_trampoline_orig as FnType as *mut c_void).unwrap();
 
             assert_eq!(test_fn_trampoline_orig(69), 42)
         }
